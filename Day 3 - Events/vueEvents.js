afddd74e351e028575d22578021eecf2b7aa94a1
Brandon Haupt <haupt.brandon@gmail.com>
2023-03-07 19:26:56 -0600

    Added new day
    - started new folder and new day

--- Day 3 - Events/vueEvents.js
+++ Day 3 - Events/vueEvents.js
@@ -5,10 +5,49 @@ const app = Vue.createApp({
         return{
             counter: 0,
             username: '',
-            confirmedName: ''
+            lastName:'',
+            // fullname: '',
         }
     },
+
+    // good for timers and http requests
+    watch:{
+        // if counter > 50 it will reset it
+        counter(value){
+            if(value > 50){
+                const that = this
+                setTimeout(function() {
+                     that.counter = 0
+                }, 2000)
+            }
+        },
+        // you repeat a data property and it will watch that whenever that property changes
+        // username(value){
+        //     console.log("running")
+        //     this.fullname = value + " " + this.lastname
+        // },
+        // lastname(value){
+        //     this.fullname = this.name + " " + value
+        // }
+    },
+
+    // will only run if it is changed 
+    // Cant use a data property
+    computed:{
+        fullname(){
+            console.log('Running')
+            if(this.username === '' || this.lastName === ''){
+                return ""
+            }
+            return this.username + ' ' + this.lastName
+        }
+    },
+
+    // will always run
     methods:{
+        outputFullName(){
+            
+        },
         add(num){
             return this.counter += num
         },
