f6a60848ecf94e99c54d9b693b19bcc96d3d1bbd
Brandon Haupt <haupt.brandon@gmail.com>
2023-03-09 19:02:01 -0600

    creating Vue Components

--- Day 6 - Components/app.js
+++ Day 6 - Components/app.js
@@ -7,21 +7,42 @@ const app = Vue.createApp({
 
     data(){
         return{
-            detailsAreVisible: false,
             friends:[
-                {
-                    id: 'manual',
-                    name: "Manual Lorenz",
-                    phone: '1234 567 8991',
-                    email: 'manuel@localhost.com'
-                },
                 {
                     id: 'julie',
                     name: "Julie Jones",
                     phone: '09876 543 221',
                     email: 'julie@localhost.com'
                 },
+                
             ],
+        }
+    },
+
+})
+
+//Components are essentially vue apps, like mini apps (they can have data, methods, etc)
+app.component('friend-contact', {
+    template: `
+    <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails()">{{detailsAreVisible ? 'Hide' : 'Show'}}</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+        </ul>
+    </li>
+    `,
+
+    data(){
+        return{
+            detailsAreVisible: false,
+            friend:{
+                id: 'manual',
+                name: "Manual Lorenz",
+                phone: '1234 567 8991',
+                email: 'manuel@localhost.com'
+            },
         }
     },
 
@@ -30,6 +51,7 @@ const app = Vue.createApp({
             this.detailsAreVisible = !this.detailsAreVisible
         },
     },
+})
 
-
-}).mount("#app")
+// If you are going to add a component then mount needs to be below everything
+app.mount("#app")
