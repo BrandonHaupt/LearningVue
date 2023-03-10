f4ab91ecd7a3533772429d28103b05c158637b90
Brandon Haupt <haupt.brandon@gmail.com>
2023-03-09 19:38:26 -0600

    Started using vue create app
    - started getting into how to create a project using Vue

---
+++ day-6_vue-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// this is how we import styles and use them in our app.vue
+import './components/styles.css'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
